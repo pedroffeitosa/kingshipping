cbdc234e8562ef782a297343a68ef0eea6aeb997
Pedro França <joao.feitosa@ccc.ufcg.edu.br>
2023-07-08 13:01:25 -0300

    simple wh added

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/calcular-frete', (req, res) => {
+  const { unidade } = req.body
+
+  // Realize o cálculo do frete com base nos dados fornecidos
+  const frete = calcularFrete(unidade)
+
+  res.json({ frete })
+})
+
+function calcularFrete(unidade) {
+  const precos = [
+    null,
+    33.85,
+    60.25,
+    86.8,
+    113.1,
+    139.25,
+    166.05,
+    190.0,
+    210.0,
+    215.0,
+    220.2,
+    240.2,
+    260.2,
+  ]
+  return precos[unidade]
+}
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Servidor em execução na porta ${port}`)
+})
